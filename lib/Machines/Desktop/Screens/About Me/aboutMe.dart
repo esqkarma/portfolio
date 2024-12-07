40fb9ddd0eacee9add1b93d21c48342479c21e11
vipinKarma <vipinkarma5@gmail.com>
2024-12-08 00:11:55 +0530

    resume error

--- lib/Machines/Desktop/Screens/About Me/aboutMe.dart
+++ lib/Machines/Desktop/Screens/About Me/aboutMe.dart
@@ -97,7 +97,7 @@ class _AboutMeState extends State<AboutMe> {
                                       child: Text(about,maxLines: 10,textAlign: TextAlign.start,textDirection: TextDirection.ltr,style: TextStyle(fontSize: 15),),
                                     )),
                                 ElevatedButton(onPressed: (){
-                                 downloadFile("https://www.bing.com/images/search?view=detailV2&ccid=%2fJ%2fzkeC5&id=2476F75F3348A29CD1B4AF1380962B5307587E33&thid=OIP._J_zkeC5KSf8SlJqk5ssowHaHX&mediaurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.fc9ff391e0b92927fc4a526a939b2ca3%3frik%3dM35YB1MrloATrw%26riu%3dhttp%253a%252f%252fupload.wikimedia.org%252fwikipedia%252fcommons%252fd%252fd4%252fCat_March_2010-1a.jpg%26ehk%3dlmx5Dow%252btE7KUtrgWeamODrZNBeLisLC4x4%252bkXQRDq8%253d%26risl%3d1%26pid%3dImgRaw%26r%3d0&exph=2212&expw=2225&q=cat&simid=608003968022873600&FORM=IRPRST&ck=34067160484FBA934C06CF66E5D81BA5&selectedIndex=3&itb=0");
+                                 downloadFile("https://drive.google.com/file/d/1qVdUzy-yn0nUv12aMPe4yqGdBl9Iigma/view?usp=sharing");
                                 }, child: Row(children: [
                                   Texts(data: "resume",),
                                   Icon(Icons.download)
